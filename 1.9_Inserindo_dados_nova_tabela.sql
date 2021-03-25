4d6d282fa161f97110382c0340be3e3fcba536ea
Italo Freitas <italorodriguesdefreitas95@gmail.com>
2021-03-25 07:24:33 -0300

    Inserindo dados na tabela relacional

---
+++ 1.9_Inserindo_dados_nova_tabela.sql
@@ -0,0 +1,14 @@
+#Obs: Como a coluna Id_Cliente se refencia a coluna de mesmo nome a tabela Clientes e preciso ser um número de Id_Cliente 
+#já existente na tabela cliente.
+
+
+insert into detalhes_aluguel values
+(default, '2', '2020-03-22', '4.90'),
+(default, '4', '2020-03-22', '4.90'),
+(default, '1', '2020-03-22', '10'),
+(default, '3', '2020-03-22', '7.90'),
+(default, '1', '2020-03-22', '9.90'),
+(default, '2', '2020-03-22', '17.90'),
+(default, '3', '2020-03-22', '5'),
+(default, '1', '2020-03-22', '16');
+
