9f522cc599f262c19e60b91618947cb338232c48
Italo Freitas <italorodriguesdefreitas95@gmail.com>
2021-03-24 21:01:34 -0300

    Adicionando dados nas tabelas

---
+++ 1.7_Andicionando_dados_nas_tabelas.sql
@@ -0,0 +1,35 @@
+#Tabela Filmes:
+
+insert into filmes values
+(default, 'Batman: O Cavaleiro das Trevas', 'Ação', '2'),
+(default, 'Ad Astra', 'Ficção Científica', '1'),
+(default, 'O Homem de Aço', 'Ação', '1'),
+(default, 'Planeta dos Macacos a guerra', 'Ficção Científica', '2'),
+(default, 'Vingadores Ultimato', 'Ação', '5'),
+(default, 'Final Fantasy VII', 'Aventura', '1'),
+(default, 'Homem Aranha Longe de Casa', 'Aventura', '3'),
+(default, 'Vingadores Guerra Infinita', 'Ação', '4'),
+(default, 'Homem Aranha de Volta ao Lar', 'Ação', '2'),
+(default, 'Rampage Destruição Total', 'Ficção Científica','1'),
+(default, 'A História de um Casamento', 'Drama','1'),
+(default, 'Venom', 'Ficção Científica','2'),
+(default, 'Batman Vs Superman', 'Ação', '3'),
+(default, '007 Cassino Royale', 'Ação', '2'),
+(default, 'Casa Comigo', 'Romance', '1'),
+(default, 'Madagascar', 'Comédia', '3');
+
+#Tabela Clientes:
+
+insert into Clientes values
+(default, 'Lucas Breno', '1994-02-08', 'M', '(85)98837-0999', 'Rua Milão 321, Manibura'),
+(default, 'Raul Freitas', '1996-06-21', 'M', '(85)98561-2342', 'Av Luiz Gonzaga 45, Cajazeiras'),
+(default, 'Daniel Mateus', '1994-05-11', 'M', '(85)97654-3210)', 'Av Reutars 42, Cajazeiras'),
+(default, 'Katia Aveiro', '1985-08-05', 'F', '(85)98765-1234', 'Rua Barcelona 134, Batata');
+
+
+#Tabela Funcionarios:
+
+insert into Funcionarios values
+(default, 'Italo Freitas', 'M', 'Rua Rainha 66, Sapiranga'),
+(default,'Raissa Dias', 'F', 'Rua Paulino 1341, Cajazeiras'),
+(default,'Arodo Lindomar', 'M', 'Rua Do Vento 34, Barroso');
